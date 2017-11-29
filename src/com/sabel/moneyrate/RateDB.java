75a000113732ce917ce81bb10292813bbb33066b
r41ns4n <rain.san@gmx.de>
2017-11-29 10:05:45 +0100

    RateDB Code optimized

--- src/com/sabel/moneyrate/RateDB.java
+++ src/com/sabel/moneyrate/RateDB.java
@@ -1,5 +1,7 @@
 package com.sabel.moneyrate;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class RateDB {
@@ -22,39 +24,67 @@ public class RateDB {
     } // END public boolean add(Rate)
 
     public boolean add(long timestamp, double rateEUR, double rateUSD) {
-        return rateList.add(timestamp, rateEUR, rateUSD);
-    }
+        Rate rateTMP = new Rate(timestamp, rateEUR, rateUSD);
+        return rateList.add(rateTMP);
+    } // END public boolean add(long timestamp, double rateEUR, double rateUSD)
 
-    private boolean checkIndex (int index) {
-        if(index >= 0 && index < size()) {
+    private boolean checkIndex(int index) {
+        if (index >= 0 && index < size()) {
             return true;
         }
         return false;
-    }
+    } // END private boolean checkIndex(int index
 
     public Rate getLastRate() {
-
-    }
+        Rate currentLastRate = null;
+        for (Rate rate : rateList) {
+            if (currentLastRate != null) {
+                if (currentLastRate.getTimestamp() <= rate.getTimestamp()) {
+                    currentLastRate = rate;
+                } // END SELECTION
+            } else {
+                currentLastRate = rate;
+            } // END FIRST RUN
+        } // END FOR
+        return currentLastRate;
+    } // END public Rate getLastRate()
 
     public Rate get(int index) {
-
-    }
+        if (!checkIndex(index)) {
+            System.out.println("Falscher Index!");
+            return null;
+        } // END IF INDEX
+        return rateList.get(index);
+    } // END public Rate get(int index)
 
     public List<Rate> get(long beginTimestamp, long endTimestamp) {
-
-    }
+        List<Rate> rateListTime = new ArrayList<>();
+        for (Rate rate : rateList) {
+            if (rate.getTimestamp() > beginTimestamp && rate.getTimestamp() < endTimestamp) {
+                rateListTime.add(rate);
+            } // END IF
+        } // END FOR
+        return rateListTime;
+    } // END public List<Rate> get(long beginTimestamp, long endTimestamp)
 
     public Rate remove(long timestamp) {
-        return rateList.remove(timestamp);
-    }
+        Iterator<Rate> iterator = rateList.iterator();
+        while (iterator.hasNext()) {
+            Rate rateTMP = iterator.next();
+            if (rateTMP.getTimestamp() == timestamp) {
+                iterator.remove();
+                return rateTMP;
+            } // END IF
+        } // END WHILE
+        return null;
+    } // END public Rate remove(long timestamp)
 
     // OVERRIDE TOSTRING
-
-
     @Override
     public String toString() {
         return "RateDB{" +
                 "RateList=" + rateList +
                 '}';
     }
+
 } // END CLASS RATEDB
